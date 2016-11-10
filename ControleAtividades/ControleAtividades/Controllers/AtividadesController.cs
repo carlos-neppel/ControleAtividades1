90478df251ab9d47c9cfaf1f4ecf48121172a9bd
Douglas Barcelos <d.barcelos@gmail.com>
2016-11-10 09:10:02 -0200

    Versão inicial do projeto

---
+++ ControleAtividades/ControleAtividades/Controllers/AtividadesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ControleAtividades.Models;
+
+namespace ControleAtividades.Controllers
+{
+    public class AtividadesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /Atividades/
+        public ActionResult Index()
+        {
+            return View(db.Atividades.ToList());
+        }
+
+        // GET: /Atividades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Atividade atividade = db.Atividades.Find(id);
+            if (atividade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atividade);
+        }
+
+        // GET: /Atividades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Atividades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="AtividadeID,Titulo,Descricao,Prioridade,StatusAtividade")] Atividade atividade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Atividades.Add(atividade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(atividade);
+        }
+
+        // GET: /Atividades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Atividade atividade = db.Atividades.Find(id);
+            if (atividade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atividade);
+        }
+
+        // POST: /Atividades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="AtividadeID,Titulo,Descricao,Prioridade,StatusAtividade")] Atividade atividade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(atividade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(atividade);
+        }
+
+        // GET: /Atividades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Atividade atividade = db.Atividades.Find(id);
+            if (atividade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(atividade);
+        }
+
+        // POST: /Atividades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Atividade atividade = db.Atividades.Find(id);
+            db.Atividades.Remove(atividade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ControleAtividades/ControleAtividades/Models/Atividade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControleAtividades.Models
+{
+    public class Atividade
+    {
+        public enum Status
+        {
+            [Display(Name="A Fazer")]
+            AFazer,
+            Fazendo,
+            Feito
+        }
+
+        public int AtividadeID { get; set; }
+
+        [Required, Display(Name="Título")]
+        public String Titulo { get; set; }
+
+        [Display(Name="Descrição")]
+        public String Descricao { get; set; }
+        
+        [Required]
+        public int Prioridade { get; set; }
+
+        [Required, Display(Name="Status")]
+        public Status StatusAtividade { get; set; }
+
+    }
+}
